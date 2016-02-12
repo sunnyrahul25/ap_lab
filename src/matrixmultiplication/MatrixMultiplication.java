99d8c7f90f90e78f547d6173987d6a722abf4d1f
Sunny Rahul <srahul.bscs13seecs@nust.edu.pk>
2016-02-12 10:44:20 +0500

    added iterative multiplication method

--- src/matrixmultiplication/MatrixMultiplication.java
+++ src/matrixmultiplication/MatrixMultiplication.java
@@ -10,6 +10,36 @@ package matrixmultiplication;
  * @author srahul.bscs13seecs
  */
 public class MatrixMultiplication {
+    
+    public static int [][] iterative_multiplication(int[][] matrixA,int[][] matrixB) {
+        // TODO code application logic here
+        int col=matrixA.length;
+        int row=matrixB[0].length;
+        if (col != row )
+            throw new IllegalArgumentException();
+        
+        int [][] result = new int [matrixA.length][matrixB[0].length];
+        
+        for (int i=0;i<result.length;i++)
+        {
+            for (int j=0;j<result[0].length;j++)
+            {
+                int sum=0;
+                for(int k=0;k<col;k++)
+                {
+                    sum+=matrixA[i][k]*matrixB[j][k];
+                }
+                result[i][j]=sum;
+            }
+            
+        }
+        return result;
+        
+    }
+    
+    public static void stassen_multiplication(int[][] matrixA,int[][] matrixB){
+        // TODO code application logic here
+    }
 
     /**
      * @param args the command line arguments
