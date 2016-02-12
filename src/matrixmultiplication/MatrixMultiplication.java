63c0f35b72e552b522c7736e72258b606b92de99
Sunny Rahul <srahul.bscs13seecs@nust.edu.pk>
2016-02-12 10:19:43 +0500

    initial commit

---
+++ src/matrixmultiplication/MatrixMultiplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrixmultiplication;
+
+/**
+ *
+ * @author srahul.bscs13seecs
+ */
+public class MatrixMultiplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
